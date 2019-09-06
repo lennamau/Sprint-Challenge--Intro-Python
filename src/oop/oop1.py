e3900596beaf574e0a28086ad9b6a5cf331cbae4
Lenna <lenna@lennamau.com>
2019-09-06 10:01:13 -0700

    oop 1 and 2 completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,37 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# Vehicle is Base Class
+
+
+class Vehicle:
+    pass
+
+# Flight:
+
+
+class FlightVehicle(Vehicle):
+    pass
+
+
+class Starship(FlightVehicle):
+    pass
+
+
+class Airplane(FlightVehicle):
+    pass
+
+# Ground
+
+
+class GroundVehicle(Vehicle):
+    pass
+
+
+class Car(GroundVehicle):
+    pass
+
+
+class Motorcycle(GroundVehicle):
+    pass
